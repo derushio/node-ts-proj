38a9550b7be5e27eabd5176ee1ee10b8af2d76cf
derushio <narumi18wa@gmail.com>
2017-07-26 22:32:00 +0900

    add install scripts

---
+++ install-npm-script.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
+
+json["scripts"] = {}
+json["scripts"]["build"] = "tsc -p src --outDir dist-es6; babel dist-es6 -d dist; rm -rf dist-es6;cp package.json dist; rsync -a node_modules dist/"
+json["scripts"]["clean"] = "rm -rf dist"
+json["scripts"]["clean-build"] = "npm run clean; tsc -p src --outDir dist-es6; babel dist-es6 -d dist; rm -rf dist-es6;cp package.json dist; cd dist; npm install"
+
+fs.writeFileSync("./package.json", JSON.stringify(json, null, 4));
---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+npm init
+
+npm install --save babel-cli babel-preset-es2015 express tsc rm ./install.sh
+node ./install-npm-script.js
+
+rm ./install-npm-script.js
