94a168816c3aea28e84820cf36f6b5efe04aee5b
derushio <narumi18wa@gmail.com>
2017-08-02 22:53:29 +0900

    fix

--- install-npm-script.js
+++ install-npm-script.js
@@ -4,7 +4,7 @@ var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
 
 json["scripts"] = {}
 json["scripts"]["build"] = "tsc -p src --outDir dist-es6; babel dist-es6 -d dist; rm -rf dist-es6;cp package.json dist; rsync -a node_modules dist/"
-json["scripts"]["clean"] = "mkdir ./dist; find ./dist -maxdepth 1 -print | grep -E \"./dist/.+\" | xargs -I{} rm -rf {}"
+json["scripts"]["clean"] = "dir=./dist; [ ! -e $dir ] && mkdir $dir; find ./dist -maxdepth 1 -print | grep -E \"./dist/.+\" | xargs -I{} rm -rf {}"
 json["scripts"]["clean-build"] = "npm run clean; tsc -p src --outDir dist-es6; babel dist-es6 -d dist; rm -rf dist-es6;cp package.json dist; cd dist; npm install"
 
 fs.writeFileSync("./package.json", JSON.stringify(json, null, 4));
