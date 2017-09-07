50a78ac7a350fc852597db438aacb6a5a95581e7
derushio <narumi18wa@gmail.com>
2017-09-07 18:40:52 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 npm init
 
-npm install --save babel-cli babel-preset-es2015 express tsc
+npm install --save babel-cli babel-preset-es2015 express typescript tsc
 node ./install-npm-script.js
 
 rm ./install.sh
