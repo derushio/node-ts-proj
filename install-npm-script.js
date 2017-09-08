e27e6aaf4726a7cf109c8d08ea4126bdf77c3db0
derushio <narumi18wa@gmail.com>
2017-09-08 16:49:38 +0900

    good bye babel

--- install-npm-script.js
+++ install-npm-script.js
@@ -3,9 +3,9 @@ const fs = require('fs')
 var json = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
 
 json["scripts"] = {}
-json["scripts"]["build"] = "tsc -p src --outDir dist-es6; babel dist-es6 -d dist; rm -rf dist-es6;cp package.json dist; rsync -a node_modules dist/"
+json["scripts"]["build"] = "tsc -p src --outDir dist;cp package.json dist; rsync -a node_modules dist/"
 json["scripts"]["clean"] = "dir=./dist; [ ! -e $dir ] && mkdir $dir; find ./dist -maxdepth 1 -print | grep -E \"./dist/.+\" | xargs -I{} rm -rf {}"
-json["scripts"]["clean-build"] = "npm run clean; tsc -p src --outDir dist-es6; babel dist-es6 -d dist; rm -rf dist-es6;cp package.json dist; cd dist; npm install"
+json["scripts"]["clean-build"] = "npm run clean; tsc -p src --outDir dist;cp package.json dist; cd dist; npm install"
 json["scripts"]["build-typedoc"] = "cd ./src; typedoc --name 'doc-server' --mode 'file' --out '../document/typedoc' './'"
 
 fs.writeFileSync("./package.json", JSON.stringify(json, null, 4));
