1e3af5b8cc36d9bbf991cb6848204dfff34b5140
derushio <narumi18wa@gmail.com>
2017-10-25 18:00:13 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
+cd src/
 npm init
+npm install --save express typescript source-map-support
+npm install --save-dev typedoc
+node ../install-npm-script.js
 
-npm install --save express typescript typedoc source-map-support
-node ./install-npm-script.js
-
+cd ../
 rm ./install.sh
 rm ./install-npm-script.js
