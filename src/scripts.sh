3e03745fbcfe4365644fec3c5d04df61d3590fd9
derushio <narumi18wa@gmail.com>
2017-12-01 22:50:37 +0900

    fix

--- src/scripts.sh
+++ src/scripts.sh
@@ -14,40 +14,25 @@ function clean() {
         xargs -I{} rm -rf {}
 }
 
-function resource_copy() {
-    rsync -a --exclude "*.ts" --exclude "tsconfig.json" --exclude "package.json" \
-        --exclude "package-lock.json" --exclude "/node_modules/" --exclude "scripts.sh" \
-        "./" "$DIST_DIR"
-}
-
 function build() {
     tsc -p "." --outDir "$DIST_DIR" --sourcemap
 }
 
 function test_build() {
-    resource_copy
     build
 
     cp "package.json" "$DIST_DIR"
-    rsync -a "node_modules" "$DIST_DIR/"
 }
 
 function clean_build() {
     clean
-    resource_copy
     build
 
     cp "package.json" "$DIST_DIR"
-    cd "$DIST_DIR" && npm install
-}
-
-function run_docker() {
-    cd "./tool/docker"
-    ./run.sh
 }
 
 function build_typedoc {
-    typedoc --excludeExternals --externalPattern "**/node_modules/**" --ignoreCompilerErrors --name "$PROJ_NAME" --mode "file" --out "../document/typedoc/" "$SRC_DIR"
+    typedoc --name "$PROJ_NAME" --mode "modules" --out "../document/typedoc" "$SRC_DIR"
 }
 
 $1
