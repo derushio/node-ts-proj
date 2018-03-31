6af210f21e13b88b521fa83b44f4d9e5424da383
derushio <narumi18wa@gmail.com>
2018-03-31 22:14:07 +0900

    [Update]

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,20 +2,12 @@ import path from 'path';
 import webpack from 'webpack';
 import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
 
-import addpage from './webpack.addpage.babel';
-
-/**
- * port
- */
-const port = 8000;
-
 /**
  * Path / File
  */
 const contextPath = path.resolve(__dirname, './');
 const distPath = path.resolve(__dirname, 'dist');
 const srcPath = path.resolve(__dirname, 'src');
-const srcPagePath = path.resolve(srcPath, 'pages');
 const entryScriptsPath = path.resolve(srcPath, 'scripts/entry');
 const outputFileName = 'bundle';
 
@@ -23,10 +15,13 @@ const outputFileName = 'bundle';
  * Webpack Config
  */
 const config = {
+    target: 'node',
     mode: process.env.NODE_ENV,
 
     context: contextPath,
-    entry: {},
+    entry: {
+        main: path.resolve(srcPath, 'main.ts')
+    },
     output: {
         path: distPath,
         filename: '[name].' + outputFileName + '.js',
@@ -34,74 +29,20 @@ const config = {
         publicPath: '/'
     },
 
-    /**
-     * webpack-dev-server config
-     * see: https://webpack.github.io/docs/webpack-dev-server.html
-     */
-    devServer: {
-        contentBase: distPath,
-        hot: true,
-        inline: true,
-        port: port
-    },
-
     resolve: {
-        extensions: ['.js', '.ts', '.json' ,'.vue'],
+        extensions: ['.js', '.ts', '.json'],
         alias: {
-            '@': path.resolve(srcPath),
-            'vue$': 'vue/dist/vue.esm.js'
+            '@': path.resolve(srcPath)
         },
     },
 
     module: {
         rules: [
-            { test: /\.html$/, loader: 'html-loader' },
-            { test: /\.pug$/, loader: 'pug-loader' },
-            { test: /\.css$/, loader:
-                process.env.NODE_ENV == 'production'?
-                    'style-loader!css-loader':
-                    'style-loader?sourceMap=true!css-loader?sourceMap=true'
-            },
-            { test: /\.sass$/, loader:
-                process.env.NODE_ENV == 'production'?
-                    'style-loader!css-loader!resolve-url-loader!sass-loader?indentedSyntax'
-                        + '&includePaths[]=src/styles':
-                    'style-loader?sourceMap=true!css-loader?sourceMap=true!'
-                        + 'resolve-url-loader!sass-loader?indentedSyntax&sourceMap=true'
-                        + '&includePaths[]=src/styles'
-            },
-            { test: /\.scss$/, loader:
-                process.env.NODE_ENV == 'production'?
-                    'style-loader!css-loader!resolve-url-loader!sass-loader'
-                        + '?includePaths[]=src/styles':
-                    'style-loader?sourceMap=true!css-loader?sourceMap=true!'
-                        + 'resolve-url-loader!sass-loader?sourceMap=true'
-                        + '&includePaths[]=src/styles'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        sass: process.env.NODE_ENV == 'production'?
-                            'vue-style-loader!css-loader!'
-                                // なぜかsassのsourcemapが必要
-                                + 'resolve-url-loader!sass-loader?indentedSyntax&sourceMap=true'
-                                + '&includePaths[]=src/styles':
-                            'vue-style-loader?sourceMap=true!css-loader?sourceMap=true!'
-                                + 'resolve-url-loader!sass-loader?indentedSyntax&sourceMap=true'
-                                + '&includePaths[]=src/styles'
-                    }
-                }
-            },
             {
                 test: /\.ts(x?)$/,
                 loader: 'ts-loader',
                 options: { appendTsSuffixTo: [ /\.vue$/ ] }
-            },
-            { test: /\.(jp(e?)g|png|gif|svg)(\?v=\d+\.\d+\.\d+)?$/, loaders: 'file-loader?name=resources/img/[name].[ext]' },
-            { test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff&name=resources/font/[name].[ext]" },
-            { test: /\.(ttf|eot)(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader?name=resources/font/[name].[ext]" }
+            }
         ]
     },
 
