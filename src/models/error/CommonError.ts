d9eeb6904d2dc9987ce26bceb57b04c6ccdf7996
derushio <narumi18wa@gmail.com>
2018-10-25 20:50:02 +0900

    <master>[deleted] rm error

--- src/models/error/CommonError.ts
+++
@@ -1,7 +0,0 @@
-import ExtendableError from '@/models/error/ExtendableError';
-
-export class CommonError extends ExtendableError<any> {
-    public constructor(message: string, extra: any = {}) {
-        super(message, extra);
-    }
-}
